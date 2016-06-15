95cece36e9ee315a4ed514e13abc84a665cb9a59
Rafael  Alves <rafael.alves@concretesolutions.com.br>
2016-06-15 20:46:46 -0300

    Cria VideoChatActivity vazio

---
+++ app/src/main/java/me/pntutorial/pnrtcblog/VideoChatActivity.java
@@ -0,0 +1,13 @@
+package me.pntutorial.pnrtcblog;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class VideoChatActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_chat);
+    }
+}
