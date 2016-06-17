3cc229f2161b8f2e35299947c3d213d495e80ce5
Rafael  Alves <rafael.alves@concretesolutions.com.br>
2016-06-17 20:53:53 -0300

    Adiciona mais componentes do tutorial para o VideoChatActivity

--- app/src/main/java/me/pntutorial/pnrtcblog/VideoChatActivity.java
+++ app/src/main/java/me/pntutorial/pnrtcblog/VideoChatActivity.java
@@ -1,14 +1,102 @@
 package me.pntutorial.pnrtcblog;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.webrtc.AudioSource;
+import org.webrtc.AudioTrack;
+import org.webrtc.MediaStream;
+import org.webrtc.PeerConnectionFactory;
+import org.webrtc.VideoCapturerAndroid;
+import org.webrtc.VideoRenderer;
+import org.webrtc.VideoRendererGui;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
+
+import me.kevingleason.pnwebrtc.PnRTCClient;
+import me.pntutorial.pnrtcblog.util.Constants;
 
 public class VideoChatActivity extends Activity {
+    public static final String VIDEO_TRACK_ID = "videoPN";
+    public static final String AUDIO_TRACK_ID = "audioPN";
+    public static final String LOCAL_MEDIA_STREAM_ID = "localStreamPN";
 
+    private PnRTCClient pnRTCClient;
+    private VideoSource localVideoSource;
+    private VideoRenderer.Callbacks localRender;
+    private VideoRenderer.Callbacks remoteRender;
+    private GLSurfaceView mVideoView;
+
+    private String username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_video_chat);
         super.onCreate(savedInstanceState);
+        // VideoChatActivity#onCreate()
+        Bundle extras = getIntent().getExtras();
+        if (extras == null || !extras.containsKey(Constants.USER_NAME)) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, "Need to pass username to VideoChatActivity in intent extras (Constants.USER_NAME).",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        this.username  = extras.getString(Constants.USER_NAME, "");
+
+        PeerConnectionFactory.initializeAndroidGlobals(
+                this,  // Context
+                true,  // Audio Enabled
+                true,  // Video Enabled
+                true,  // Hardware Acceleration Enabled
+                null); // Render EGL Context
+
+        PeerConnectionFactory pcFactory = new PeerConnectionFactory();
+        this.pnRTCClient = new PnRTCClient(Constants.PUB_KEY, Constants.SUB_KEY, this.username);
+
+
+        // VideoChatActivity#onCreate()
+// Returns the number of cams & front/back face device name
+        int camNumber = VideoCapturerAndroid.getDeviceCount();
+        String frontFacingCam =  VideoCapturerAndroid.getNameOfFrontFacingDevice();
+        String backFacingCam  = VideoCapturerAndroid.getNameOfBackFacingDevice();
+
+// Creates a VideoCapturerAndroid instance for the device name
+        VideoCapturerAndroid capturer = (VideoCapturerAndroid) VideoCapturerAndroid.create(frontFacingCam);
+
+// First create a Video Source, then we can make a Video Track
+        localVideoSource = pcFactory.createVideoSource(capturer, this.pnRTCClient.videoConstraints());
+        VideoTrack localVideoTrack = pcFactory.createVideoTrack(VIDEO_TRACK_ID, localVideoSource);
+
+// First we create an AudioSource then we can create our AudioTrack
+        AudioSource audioSource = pcFactory.createAudioSource(this.pnRTCClient.audioConstraints());
+        AudioTrack localAudioTrack = pcFactory.createAudioTrack(AUDIO_TRACK_ID, audioSource);
+
+        // VideoChatActivity#onCreate()
+        MediaStream mediaStream = pcFactory.createLocalMediaStream(LOCAL_MEDIA_STREAM_ID);
+
+// Now we can add our tracks.
+        mediaStream.addTrack(localVideoTrack);
+        mediaStream.addTrack(localAudioTrack);
+
+        // Then we set that view, and pass a Runnable to run once the surface is ready
+        VideoRendererGui.setView(mVideoView, null);
+
+// Now that VideoRendererGui is ready, we can get our VideoRenderer.
+// IN THIS ORDER. Effects which is on top or bottom
+        remoteRender = VideoRendererGui.create(0, 0, 100, 100, VideoRendererGui.ScalingType.SCALE_ASPECT_FILL, false);
+        localRender = VideoRendererGui.create(0, 0, 100, 100, VideoRendererGui.ScalingType.SCALE_ASPECT_FILL, true);
+
+        // Then we set that view, and pass a Runnable to run once the surface is ready
+        VideoRendererGui.setView(mVideoView, null);
+
+// Now that VideoRendererGui is ready, we can get our VideoRenderer.
+// IN THIS ORDER. Effects which is on top or bottom
+        remoteRender = VideoRendererGui.create(0, 0, 100, 100, VideoRendererGui.ScalingType.SCALE_ASPECT_FILL, false);
+        localRender = VideoRendererGui.create(0, 0, 100, 100, VideoRendererGui.ScalingType.SCALE_ASPECT_FILL, true);
 
     }
 }
